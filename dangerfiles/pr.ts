f58d72dba8d0ea6e5e8ba6a0445c4e6ad5e65bfb
Jimmy McDermott <mcdappdev@gmail.com>
2018-10-13 13:17:52 -0400

    update PR message

--- dangerfiles/pr.ts
+++ dangerfiles/pr.ts
@@ -1,3 +1,3 @@
 import { message } from "danger"
 
-message("Test");
+message("Hey! Thanks for opening a PR. Please make sure that you included tests. If you want to merge this branch upon all CI runs passing, comment 'merge on green.'");
