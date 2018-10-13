0f3d3bac28d0aabbfe3d95f4150815ee08a1b60b
Jimmy McDermott <mcdappdev@gmail.com>
2018-10-13 13:21:00 -0400

    try nothign

--- dangerfiles/pr.ts
+++ dangerfiles/pr.ts
@@ -1,3 +1,1 @@
 import { message } from "danger"
-
-message("Hey! Thanks for opening a PR. Please make sure that you included tests. If you want to merge this branch upon all CI runs passing, comment 'merge on green.'");
